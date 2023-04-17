a5971afca2beb6de70c5a79881d6f769e6298003
Hrishikesh Kukade <hrishikeshkukade6@gmail.com>
2023-04-17 18:15:22 +0530

    Added to git

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import SignUp from './Components/SignUp';
 import SignIn from "./Components/SignIn";
 function App() {
   return (
     <div className="App">
-      <SignIn />  
+      <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
--- my-app/src/Components/SignUp.js
+++ my-app/src/Components/SignUp.js
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
-import classes from "./SignIn.module.css";
-
+import React, {useState} from "react";
+import classes from "./SignUp.module.css";
+import CheckboxWithAnimation from "./Checkbox";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-const SignIn = () => {
-  const [signin, setSignin] = useState(true);
-  
-  const [showPassword, setShowPassword] = useState(false);
+import { Link } from "react-router-dom";
+const SignUp = () => {
+    const [isChecked, setIsChecked] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
-  const stateChangeHandler = () => {
-    setSignin(!signin);
-  };
-  
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
+
+    const checkChangeHandler = (e) => {
+        setIsChecked(e.target.checked);
+      }
+      const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+      };
   return (
     <React.Fragment>
       <div className={classes.signin}>
         <div className={classes.container}>
           <div className={classes.info}>
-            <h1>Login</h1>
-            <h1>Hi, Welcome back</h1>
-            <h5>You can continue to login to manage your events</h5>
+            <h1>Setup your profile</h1>
+           
+            <h5>Create an account so you can create and manage all your events at one place</h5>
           </div>
           <div className={classes.allinputs}>
             <form className={classes.form}>
-              
+             
                 <div className={classes.email}>
                   <h3>Name</h3>
                   <input placeholder="Enter Name" type="text"></input>
@@ -40,25 +40,29 @@ const SignIn = () => {
                 <h3>Password</h3>
                 <input
                   placeholder="Enter a password..."
-                  type={showPassword ? 'text' : 'password'}
+                  type={showPassword ? "text" : "password"}
                 ></input>
-                 <FontAwesomeIcon
-        icon={showPassword ?  faEyeSlash  :  faEye}
-         
-        onClick={togglePasswordVisibility}
-        className={classes.icon}
-      />
+                <FontAwesomeIcon
+                  icon={showPassword ? faEyeSlash : faEye}
+                  onClick={togglePasswordVisibility}
+                  className={classes.icon}
+                />
               </div>
-              
+               
                 <div className={classes.password}>
                   <h3>Confirm Password</h3>
                   <input
                     placeholder="Confirm the entered password..."
                     type="password"
                   ></input>
+                   <FontAwesomeIcon
+                  icon={showPassword ? faEyeSlash : faEye}
+                  onClick={togglePasswordVisibility}
+                  className={classes.icon}
+                />
                 </div>
               
-              
+             
                 <div className={classes.password}>
                   <h3>Mobile Number</h3>
                   <input
@@ -67,24 +71,29 @@ const SignIn = () => {
                   ></input>
                 </div>
               
-             
-                <div className={classes.text}>
-                  <h6>Forgot Password</h6>
-                </div>
               
+                
+              
+            
+                <CheckboxWithAnimation
+                  isChecked={isChecked}
+                  onChange={checkChangeHandler}
+                />
               
             </form>
 
             <div>
               <button className={classes.button}>
-                {signin ? "Sign-in" : "Register"}
+                Register
               </button>
             </div>
             <div className={classes.bottomtext}>
+              <Link to="/signin">
               <h5>
-                Don't have an account?{" "}
-                <span onClick={stateChangeHandler}>Sign Up</span>
+                Already have an account?{" "}
+                <span>Sign In</span>
               </h5>
+              </Link>
             </div>
           </div>
         </div>
@@ -93,4 +102,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default SignUp;
