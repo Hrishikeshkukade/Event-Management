ca3d8aa40a75d7e1c50d55c7cde0112fea039b2d
Hrishikesh Kukade <hrishikeshkukade6@gmail.com>
2023-04-24 21:24:13 +0530

    Added

--- my-app/src/Components/Settings.js
+++ my-app/src/Components/Settings.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Header from "../UI/Header";
 import classes from "./Settings.module.css";
-import { faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getAuth, signOut } from "firebase/auth";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Settings = () => {
+    const auth = getAuth();
+    const history = useHistory();
+    const signOutHandler = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            history.push("/");
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
     return(
     <React.Fragment> 
         <Header />
@@ -12,11 +25,11 @@ const Settings = () => {
         <div className={classes.profile}>
             <h2>Update Profile</h2>
         </div>
-        <FontAwesomeIcon className={classes.icon} icon={faCircleChevronRight}/>
+        {/* <FontAwesomeIcon className={classes.icon} icon={faCircleChevronRight}/> */}
         <div>
         </div>
             
-        <div className={classes.logout}>
+        <div onClick={signOutHandler} className={classes.logout}>
             <h2>Log Out</h2>
         </div>
     </div>      
